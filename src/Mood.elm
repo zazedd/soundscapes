59aa80571766de7a49195e4fa0170d6aec6a786c
zazed <zazed@localhost.localdomain>
2023-05-11 16:38:33 +0100

    divs overlap as they should

--- src/Mood.elm
+++ src/Mood.elm
@@ -63,47 +63,49 @@ mood model =
             ]
         , div [ class "main-content" ]
             [ h1 [ id "title" ] [ text "soundscapes" ]
-            , div [ classList [ ( "center-content", True ), ( "visible", model.divvis.visible1 ) ] ]
-                [ p [] [ text "How are you feeling?" ]
-                , div [ class "range" ]
-                    [ div [ class "label-left" ]
-                        [ label [ for "emotionalRange", class "form-label" ]
-                            [ span [ class "material-symbols-outlined" ] [ text "sentiment_dissatisfied" ]
+            , div [ class "center-content" ]
+                [ div [ classList [ ( "range-div", True ), ( "visible", model.divvis.visible1 ) ] ]
+                    [ p [] [ text "How are you feeling?" ]
+                    , div [ class "range" ]
+                        [ div [ class "label-left" ]
+                            [ label [ for "emotionalRange", class "form-label" ]
+                                [ span [ class "material-symbols-outlined" ] [ text "sentiment_dissatisfied" ]
+                                ]
                             ]
-                        ]
-                    , input
-                        [ type_ "range"
-                        , class "form-range"
-                        , Html.Attributes.min "0"
-                        , Html.Attributes.max "5"
-                        , step "0.5"
-                        , id "emotionalRange"
-                        , disabled (not model.divvis.visible1)
-                        ]
-                        []
-                    , div [ class "label-right" ]
-                        [ label [ for "emotionalRange", class "form-label" ]
-                            [ span [ class "material-symbols-outlined" ] [ text "sentiment_excited" ]
+                        , input
+                            [ type_ "range"
+                            , class "form-range"
+                            , Html.Attributes.min "0"
+                            , Html.Attributes.max "5"
+                            , step "0.5"
+                            , id "emotionalRange"
+                            , disabled (not model.divvis.visible1)
+                            ]
+                            []
+                        , div [ class "label-right" ]
+                            [ label [ for "emotionalRange", class "form-label" ]
+                                [ span [ class "material-symbols-outlined" ] [ text "sentiment_excited" ]
+                                ]
                             ]
                         ]
                     ]
-                ]
-            , div [ classList [ ( "center-content", True ), ( "visible", model.divvis.visible2 ) ] ]
-                [ p [] [ text "What genre are you in the mood for?" ]
-                , div [ class "range" ]
-                    [ select
-                        [ class "form-select"
-                        , ariaLabel "Pick your genre"
-                        , id "genreInput"
-                        , disabled (not model.divvis.visible2)
-                        ]
-                        [ option [ selected True ] [ text "Rock" ]
-                        , option [ value "1" ] [ text "Electronic" ]
-                        , option [ value "2" ] [ text "Jazz" ]
-                        , option [ value "3" ] [ text "Pop" ]
-                        , option [ value "4" ] [ text "Hip-hop" ]
-                        , option [ value "5" ] [ text "Metal" ]
-                        , option [ value "5" ] [ text "Indie" ]
+                , div [ classList [ ( "select-div", True ), ( "visible", not model.divvis.visible1 ) ] ]
+                    [ p [] [ text "What genre are you in the mood for?" ]
+                    , div [ class "range" ]
+                        [ select
+                            [ class "form-select"
+                            , ariaLabel "Pick your genre"
+                            , id "genreInput"
+                            , disabled model.divvis.visible1
+                            ]
+                            [ option [ selected True ] [ text "Rock" ]
+                            , option [ value "1" ] [ text "Electronic" ]
+                            , option [ value "2" ] [ text "Jazz" ]
+                            , option [ value "3" ] [ text "Pop" ]
+                            , option [ value "4" ] [ text "Hip-hop" ]
+                            , option [ value "5" ] [ text "Metal" ]
+                            , option [ value "5" ] [ text "Indie" ]
+                            ]
                         ]
                     ]
                 ]
